5bce5c2c46eba7e5e7f416d52bbfa2ecd32c5d34
umesh jyani <49265224+Umeshjyani@users.noreply.github.com>
2023-07-20 08:35:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+function updateDateTime() {
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  let hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+
+  // Convert hours to 12-hour format and determine AM or PM
+  let amOrPm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12;
+
+  // Update the HTML elements with the current date and time
+  document.getElementById('current-date').textContent = `${month}-${day}-${year}`;
+  document.getElementById('current-time').textContent = `${hours}:${formatTimeComponent(minutes)}:${formatTimeComponent(seconds)} ${amOrPm}`;
+}
+
+// Function to add leading zero to single-digit numbers
+function formatTimeComponent(timeComponent) {
+  return timeComponent < 10 ? `0${timeComponent}` : timeComponent;
+}
+
+// Call the function to update the date and time initially
+updateDateTime();
+
+// Update the date and time every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
